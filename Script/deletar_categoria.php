a08b63a1b5c3f718c25a88e6f2766960d337fbfc
Daniel L. Alves <daniel@clinicarx.com.br>
2020-05-19 20:07:15 -0300

    Insere opção de deletar categoria

---
+++ Script/deletar_categoria.php
@@ -0,0 +1,23 @@
+<?php 
+
+include 'conexao.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM `categoria` WHERE id_categoria = $id";
+
+$deletar = mysqli_query($conexao,$sql);
+?>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<div class="container" style="width: 400px;"></div>
+<center>
+    <div style="margin-top: 30px;">
+        <h3>Categoria excluída com sucesso</h3>
+    </div>
+    <div style="margin-top: 20px;">
+        <a href="listar_categorias.php" class="btn btn-sm btn-primary">Ir para lista de produtos</a>
+    </div>
+</center>
